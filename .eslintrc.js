5fd3d6acc924e3e4f06c1c49ea5fcf216d1ae22b
Vaibhav Mishra <vaibhav@geekflare.com>
2026-06-04 11:52:22 +0530

    updated dependencies

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended"],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
-  },
-};
